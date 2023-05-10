bdd50c579a424a819cec3bb166600960c925755c
Yibrah Teare <112482941+yobocode@users.noreply.github.com>
2023-05-10 14:19:31 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import sys
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    headers = {"User-Agent": 'xica369'}
+    headers = {"User-Agent": 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'limit': 10}
     response = requests.get(url, headers=headers, allow_redirects=False, 
