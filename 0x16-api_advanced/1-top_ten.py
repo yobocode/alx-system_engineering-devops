965a12124db15a9e0e44cef53a8b16249ed399e1
Yibrah Teare <112482941+yobocode@users.noreply.github.com>
2023-05-10 14:16:43 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,13 +5,13 @@ import sys
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    headers = {"User-Agent": 'xica369'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {"limit": 10}
+    parameters = {'limit': 10}
     response = requests.get(url, headers=headers, allow_redirects=False, 
                            params=parameters)
     if response.status_code == 200:
-        titles = response.json().get("data").get("children")
+        titles = response.json().get('data').get('children')
         for title_ in titles_:
             print(title_.get('data').get('title'))
      else:
