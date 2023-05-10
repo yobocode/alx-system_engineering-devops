749b118437bd34a4f4d6cdd46f70a84d8d5aa09e
Yibrah Teare <112482941+yobocode@users.noreply.github.com>
2023-05-10 14:20:35 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,16 +3,20 @@
 import requests
 import sys
 
+
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    headers = {"User-Agent": 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'limit': 10}
-    response = requests.get(url, headers=headers, allow_redirects=False, 
-                           params=parameters)
-    if response.status_code == 200:
-        titles = response.json().get('data').get('children')
-        for title_ in titles_:
-            print(title_.get('data').get('title'))
-     else:
-        print(None)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
