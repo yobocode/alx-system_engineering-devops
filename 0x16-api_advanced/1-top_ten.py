d5215b9454c87cc3b62e0c0149187d5c183a0c22
Yibrah Teare <112482941+yobocode@users.noreply.github.com>
2023-05-12 10:31:55 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+'''A module containing function for working with reddit api.
+'''
 import requests
-import sys
+
+
+URL = 'https://www.reddit.com'
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    '''Retrives the tittle of top ten posts of the given subreddit.
+    '''
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+            'Accept': 'application/json',
+            'User-Agent': ' '.join([
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+                'AppleWebKit/537.36 (KHTML, like Gecko)',
+                'Chrome/112.0.0.0',
+                'Safari/537.36'
+                ])
+            }
+    result = requests.get(
+            '{}/r/{}/hot.json?sort=top&limit=10'.format(URL, subreddit),
+            headers=headers,
+            allow_redirects=False
+            )
+    if result.status_code == 200:
+        posts = result.json()['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
